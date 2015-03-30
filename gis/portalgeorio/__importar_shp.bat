de342a485b58ef9668c17a1956524f472dae00be
Sérgio Rodrigues <srodriguex@gmail.com>
2015-03-29 21:58:20 -0300

    Ímportar shapefiles

--- gis/portalgeorio/__importar_shp.bat
+++ gis/portalgeorio/__importar_shp.bat
@@ -1,2 +1,2 @@
-for %%f in (*.shp) do shp2pgsql -d -W LATIN1 -I -s 29193:4326 %%f pgr_%%~nf > pgr_%%~nf.sql
+for %%f in (*.shp) do shp2pgsql -W LATIN1 -I -s 29193:4326 %%f pgr_%%~nf > pgr_%%~nf.sql
 for %%f in (*.sql) do psql -d postgresql://postgres:1234@localhost/zap -f %%f
